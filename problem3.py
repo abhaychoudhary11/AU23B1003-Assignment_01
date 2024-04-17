cf0996548fb90a9d09791db96eadbb2f60249630
ABHAY CHOUDHARY <156689021+abhaychoudhary11@users.noreply.github.com>
2024-04-17 23:52:45 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,5 +1,10 @@
+x=int(input("no. of order you want:"))
+orders = []
+for i in range(x):
+    if x>1:
+        y=input("dish:")
+        orders.append(y)
 def restaurent(order):
-    orders = ["french fries", "Pani puri", "burger"]
     for i in range(len(orders)):
         print("preparing your order:",orders[i])
     print("the following orders have been dispatched:")
